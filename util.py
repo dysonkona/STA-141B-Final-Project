892ac4320073a9fcb70d5bf82b484a87ab4bcc94
JeremyElvander <jeremyelvander@gmail.com>
2024-11-18 20:39:31 -0800

    util file

--- util.py
+++ util.py
@@ -1,4 +1,38 @@
-#Coordinates finder
+import requests
+import json
+from json import loads
+import pandas as pd
+from datetime import datetime
+import time
+
+
+def stationLoad(cities):
+    url = "https://api.bart.gov/api/stn.aspx"  
+    key = "Z2XJ-5YDK-9AKT-DWEI"
+    response = requests.get(url, params={
+    "key": key,  # outcoded personal legacy key
+    "cmd": "stns",
+    "json": "y"
+    
+    })  
+    time.sleep(2) #maybe use for loop if make mulitple requestst
+
+    if response.status_code == 200:
+        data = response.json()  # Parse JSON response
+    else:
+        print("Error:", response.status_code)
+    
+    data = data["root"]["stations"]["station"] #long list of nested dicts, get down
+    sfstations = [] #initialize empty list of sf stations
+    for station in data:
+    # Check if the city is "San Francisco"
+        if station.get("city") in cities:
+            sfstations.append(station)
+
+    df = pd.DataFrame(sfstations)[['name', 'city', 'gtfs_latitude', 'gtfs_longitude']].sort_values("city").reset_index()
+    df.drop('index', axis = 1, inplace = True)
+
+    return df
 
 def crimeRadius():
     raise NotImplementedError
