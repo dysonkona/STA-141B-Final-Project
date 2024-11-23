31f9122c135aeb39cec15495be6b6a6b92ae8ed9
JeremyElvander <jeremyelvander@gmail.com>
2024-11-23 12:35:41 -0800

    finished

--- processing.py
+++ processing.py
@@ -8,7 +8,10 @@ import functools
 def subProcess(data, operations, metadata):
     for operation, args in zip(operations, metadata):
         if args is not None:
-            data = operation(data, args)
+            if type(args) == list:
+                data = operation(data, *args)
+            else:
+                data = operation(data, args)
         else:
             data = operation(data)
     return data
@@ -18,33 +21,42 @@ def Pagination(query, fetch_function, maxRecords, user, password):
     limit = query[4]  # Assuming 'limit' is the 5th element in the query
     offset = query[5]  # Assuming 'offset' is the 6th element in the query
     all_data = []
+    tp = []
     while True:
+        s = time.time()
         query[4] = limit
         query[5] = offset
         page_data = fetch_function(*query, user, password)
         if not page_data:  # Stop if no more data
             break
         all_data.extend(page_data)
-
+        e = time.time()
         if maxRecords is not None and len(all_data) >= maxRecords:
-            return all_data[:maxRecords]
-
+            tp.append((e-s, len(page_data)))
+            return all_data[:maxRecords], tp
+        tp.append((e-s, len(page_data)))
         offset += limit
-    return all_data
+    return all_data, tp
 
 def parallelProcess(workers, function, params, task = 'load', 
                     nested = True, maxRecords = None, subprocess = None, metadata = None, *args):
+    throughput = []
     def fetchQuery(query):
         if nested:
-            return Pagination(query, function, maxRecords, *args)  
-        return function(query, *args)
+            data, through = Pagination(query, function, maxRecords, *args)  
+        else:
+            data = function(query, *args)
+
+        throughput.append(through)
+        return data
     
     def processAll(args):
         """Process all paginated data for a station."""
+        
         query, meta = args
         data = fetchQuery(query)  # Fetch all pages or data
         if subprocess:
-            return subProcess(data, subprocess, [meta])
+            return subProcess(data, subprocess, meta)
         return data
     
     startTime = time.time()
@@ -60,11 +72,12 @@ def parallelProcess(workers, function, params, task = 'load',
         # else:
         #     results = rawResults
         endTime = time.time()
-        print("Time taken to process (seconds):", str(endTime-startTime))
+        final = endTime - startTime
+        print("Time taken to process (seconds):", str(final))
         if task == 'load':
             flattened = [value for sublist in results for value in sublist]
-            return pd.DataFrame(flattened)
+            return pd.DataFrame(flattened), throughput, final
         if task == 'other':
-            return results
+            return results, throughput, final
 
 
